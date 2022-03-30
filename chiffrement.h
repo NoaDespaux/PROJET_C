d523fbcc650bea1115d928d135289f784d318f2f
NoaDespaux <102654073+NoaDespaux@users.noreply.github.com>
2022-03-30 11:09:13 +0200

    fonctions de base

---
+++ chiffrement.h
@@ -0,0 +1,9 @@
+boolean verifierAlphanumerique(char * texte);
+
+void convertirAccents(char * texte);
+
+void chiffrer(char * texte, int cle);
+
+void déchiffrer(char * texte, int cle);
+
+void affichage(char * texte);
