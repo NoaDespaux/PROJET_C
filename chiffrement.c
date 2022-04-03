209e8598a3cec7fbb8bba997cef53bf123c252cb
NoaDespaux <102654073+NoaDespaux@users.noreply.github.com>
2022-04-03 17:55:05 +0200

    verifierAlphanumerique v1

--- chiffrement.c
+++ chiffrement.c
@@ -24,4 +24,30 @@
 *                                                                             *
 ******************************************************************************/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "chiffrement.h"
+
+
+//Caractères autorisés : A-Z a-z 0-9 ',' '-' '.' '/'
+
+int verifierAlphanumerique(char * texte){
+	for(int i = 0; i < strlen(texte); i++){
+		if (texte[i] < 32){
+			return 0;
+		}
+		if (texte[i] >= 33 && texte[i] <=43 ){
+			return 0;
+		}
+		if (texte[i] >= 58 && texte[i] <= 64 ){
+			return 0;
+		}
+		if (texte[i] >= 91 && texte[i] <= 96 ){
+			return 0;
+		}
+		if (texte[i] > 122){
+			return 0;
+		}
+	}
+	return 1;
+}
